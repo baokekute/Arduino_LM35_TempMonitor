536e0df5955538f9509e3959fe41877e19cb8d0c
baokekute <baokekute0001@gmail.com>
2026-04-14 00:48:47 +0700

    chore: reset code goc 2 kenh

--- firmware/LM35_TempReader/LM35_TempReader.ino/LM35_TempReader.ino.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino/LM35_TempReader.ino.ino
@@ -1,14 +1,15 @@
-const int sensorPins[3] = {A0, A1, A2}; // 3 kênh
+const int sensorPins[2] = {A0, A1};
 void setup() { Serial.begin(9600); }
 void loop() {
-  int adcValues[3]; float tempC[3];
-  for(int i=0; i<3; i++) {
+  int adcValues[2]; float tempC[2];
+  for(int i=0; i<2; i++) {
     adcValues[i] = analogRead(sensorPins[i]);
     tempC[i] = (adcValues[i] * 500.0) / 1023.0;
   }
-  // Vẫn in CSV nhưng có thêm kênh 3
-  Serial.print(tempC[0]); Serial.print(",");
-  Serial.print(tempC[1]); Serial.print(",");
-  Serial.println(tempC[2]);
+  // --- DOAN CODE NAY SE BI XUNG DOT ---
+  Serial.print(tempC[0]);
+  Serial.print(",");
+  Serial.println(tempC[1]);
+  // ------------------------------------
   delay(1000);
 }
