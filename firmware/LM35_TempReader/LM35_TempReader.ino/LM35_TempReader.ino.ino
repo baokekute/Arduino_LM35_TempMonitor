7d85b63fc0b5cd1266ad1d7206756b6224298269
baokekute <baokekute0001@gmail.com>
2026-04-14 00:39:21 +0700

    feat: doi dinh dang Serial sang JSON

--- firmware/LM35_TempReader/LM35_TempReader.ino/LM35_TempReader.ino.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino/LM35_TempReader.ino.ino
@@ -1,14 +1,25 @@
-const int sensorPins[3] = {A0, A1, A2}; // 3 kênh
-void setup() { Serial.begin(9600); }
+const int sensorPins[2] = {A0, A1}; // Vẫn giữ nguyên 2 kênh ban đầu
+
+void setup() {
+  Serial.begin(9600);
+}
+
 void loop() {
-  int adcValues[3]; float tempC[3];
-  for(int i=0; i<3; i++) {
+  int adcValues[2];
+  float tempC[2];
+
+  // Vòng lặp đọc 2 kênh
+  for(int i=0; i<2; i++) {
     adcValues[i] = analogRead(sensorPins[i]);
     tempC[i] = (adcValues[i] * 500.0) / 1023.0;
   }
-  // Vẫn in CSV nhưng có thêm kênh 3
-  Serial.print(tempC[0]); Serial.print(",");
-  Serial.print(tempC[1]); Serial.print(",");
-  Serial.println(tempC[2]);
+
+  // In ra Serial dạng chuỗi JSON chuẩn
+  Serial.print("{\"temp1\": ");
+  Serial.print(tempC[0]);
+  Serial.print(", \"temp2\": ");
+  Serial.print(tempC[1]);
+  Serial.println("}");
+
   delay(1000);
 }
